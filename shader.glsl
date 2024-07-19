d8701c8277e5e554d9674482977aaf5cd4bd1065
Hugo Huart <hugo.huart@heig-vd.ch>
2024-07-19 14:08:08 +0200

    Shader overhaul

--- shader.glsl
+++ shader.glsl
@@ -2,11 +2,32 @@
 
 in vec2 fragTexCoord;
 
+uniform int palette;
 uniform int maxiter;
 uniform vec2 x;
 uniform vec2 y;
 
 out vec4 finalColor;
+
+vec2 cpow2(vec2 c) {
+    return vec2(
+        c.x * c.x - c.y * c.y,
+        2.0 * c.x * c.y        
+    );
+}
+
+vec3 bwpalette(int i) {
+    float c = (i % 64) / 64.0;
+    return vec3(c, c, c);
+}
+
+vec3 rgbpalette(int i) {
+    return vec3(
+        (i % 32) / 32.0,
+        (i % 64) / 64.0,
+        (i % 96) / 96.0
+    );
+}
 
 void main() {
     float cr = x.x + fragTexCoord.x * (x.y - x.x);
@@ -15,16 +36,18 @@ void main() {
     float zr = 0.0;
     float zi = 0.0;
 
-    for (float i = 0; i < maxiter; ++i) {
-        float nzr = zr * zr - zi * zi + cr;
-        float nzi = 2.0 * zr * zi + ci;
+    for (int i = 0; i < maxiter; ++i) {
+        vec2 z = cpow2(vec2(zr, zi));
 
-        zr = nzr;
-        zi = nzi;
+        zr = z.x + cr;
+        zi = z.y + ci;
 
         if (zr * zr + zi * zi > 4.0) {
-            float c = i / maxiter;
-            finalColor = vec4(c, c, c, 1.0);
+            if (palette == 0) {
+                finalColor = vec4(bwpalette(i), 1.0);
+            } else {
+                finalColor = vec4(rgbpalette(i), 1.0);
+            }
             return;
         }
     }
